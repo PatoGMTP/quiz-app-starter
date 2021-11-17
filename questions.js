d5b227a478ce52e3f51ca0f1d45c94bcd4b59498
PatoGMTP <93350228+PatoGMTP@users.noreply.github.com>
2021-11-17 15:01:11 -0600

    Quiz functioning, adding builder next

--- questions.js
+++ questions.js
@@ -1,1 +1,62 @@
 //Your quiz questions array of objects goes here
+
+class question
+{
+    prompt;
+    choices;
+    answer;
+
+    constructor(p, c, a)
+    {
+        this.prompt = p;
+        this.choices = c;
+        this.answer = a;
+    }
+}
+
+// let p1 = "At the beginning of the poem, the 'Odyssey', which Greek god is angry with Odysseus?";
+// let p2 = "In the novel 'To Kill a Mockingbird', the main character's father, Atticus Finch, has a key role as a:";
+// let p3 = "In the novel, 'Brave New World', everyone is dependent on a special happiness-producing drug called:";
+// let p4 = "In the novel, 'A Christmas Carol', which ghost does Scrooge not face?";
+// let p5 = "In the novel, 'The Death of Arthur', King Arthur is betrayed and killed by:";
+// let prompts = [p1, p2, p3, p4, p5];
+
+// let c1 = ["Poseiodon", "Athena", "Zeus", "Circe"];
+// let c2 = ["Soldier", "Teacher", "Lawyer", "Sheriff"];
+// let c3 = ["Allevio", "Soma", "Pase", "Limbo"];
+// let c4 = ["Ghost of Christmas Past", "Ghost of Christmas Yet to Come", "Ghost of Christmas Spirit", "Ghost of Christmas Present"];
+// let c5 = ["Lancelot", "Merlin", "Bedivere", "Mordred"];
+// let choices = [c1, c2, c3, c4, c5];
+
+// let a1 = "Poseidon";
+// let a2 = "Lawyer";
+// let a3 = "Soma";
+// let a4 = "Ghost of Christmas Spirit";
+// let a5 = "Mordred";
+// let answers = [a1, a2, a3, a4, a5];
+
+let starter_questions = [];
+
+// for (let i = 0; i < 5; i++)
+// {
+//     starter_questions.push(new question(prompts[i], choices[i], answers[i]));
+// }
+
+starter_questions = [new question("A", ["A", "B", "C"], "A"), new question("B", ["X", "Y", "Z", "0"], "0")];
+
+let storage = localStorage.getItem("questions");
+
+let quiz;
+
+if (storage === null)
+{
+    quiz = starter_questions;
+}
+else
+{
+    quiz = JSON.parse(storage);
+}
+
+console.log(quiz);
+
+export {quiz};
--- quiz.js
+++ quiz.js
@@ -1,1 +1,190 @@
 //Your quiz functionality goes here
+import { quiz } from "./questions.js";
+
+let question;
+let current = -1;
+
+let quiz_title;
+let quiz_stats;
+let quiz_prompt;
+let quiz_choice_list;
+let quiz_respond_button;
+let quiz_results_list;
+let quiz_start_button;
+let quiz_score;
+let quiz_retry_button;
+let quiz_body;
+let quiz_exit_button;
+let quiz_build_button;
+
+let responses = [];
+
+function main()
+{
+    quiz_title = document.getElementById("quiztitle");
+    quiz_stats = document.getElementById("stats");
+    quiz_prompt = document.getElementById("prompt");
+    quiz_choice_list = document.getElementById("choices");
+    quiz_respond_button = document.getElementById("respond");
+    quiz_results_list = document.getElementById("results");
+    quiz_start_button = document.getElementById("start");
+    quiz_score = document.getElementById("score");
+    quiz_retry_button = document.getElementById("retry");
+    quiz_body = document.getElementById("quizbody");
+    quiz_exit_button = document.getElementById("exit");
+    quiz_build_button = document.getElementById("build");
+
+    quiz_respond_button.addEventListener("click", nextQuestion);
+
+    quiz_start_button.addEventListener("click", startQuiz);
+
+    quiz_retry_button.addEventListener("click", retryQuiz);
+
+    quiz_exit_button.addEventListener("click", exitQuiz);
+
+    quiz_build_button.addEventListener("click", console.log);
+
+    quiz_title.innerHTML = "Lit Quiz!";
+
+    render();
+}
+
+function startQuiz()
+{
+    current = 0;
+    quiz_start_button.hidden = true;
+    quiz_retry_button.hidden = true;
+    quiz_build_button.hidden = true;
+    quiz_exit_button.hidden = false;
+    quiz_body.hidden = false;
+    Array.from( quiz_body.children ).forEach(item=> item.hidden = false);
+
+    shuffleArray(quiz);
+
+    render();
+}
+
+function retryQuiz()
+{
+    responses = [];
+    quiz_results_list.hidden = true;
+    quiz_score.hidden = true;
+    startQuiz();
+}
+
+function exitQuiz()
+{
+    current = -1;
+    responses = [];
+    quiz_results_list.hidden = true;
+    quiz_score.hidden = true;
+    quiz_exit_button.hidden = true;
+    quiz_start_button.hidden = false;
+    render();
+}
+
+function displayCurrentQuestion()
+{
+    quiz_prompt.innerHTML = question.prompt;
+    quiz_choice_list.innerHTML = "";
+
+    question.choices.forEach(item => {
+        let label = document.createElement("label");
+        label.innerHTML = item;
+        let radio = document.createElement("input");
+        radio.type = "radio";
+        radio.name = "choice";
+        label.appendChild(radio);
+        quiz_choice_list.appendChild(label);
+    });
+}
+
+function nextQuestion()
+{
+    let chosen = quiz_choice_list.querySelector("input:checked");
+    
+    if (chosen === null)
+    {
+        alert("Please enter an answer!");
+    }
+    else
+    {
+        responses.push(chosen.parentElement.innerText);
+        current++;
+        quiz_prompt.innerHTML = "";
+        quiz_choice_list.innerHTML = "";
+        render();
+    }
+}
+
+function displayResults()
+{
+    let correct = 0;
+
+    quiz_results_list.innerHTML = "";
+
+    quiz.forEach((item, i)=> {
+        let li = document.createElement("li");
+        li.innerHTML = item.prompt;
+
+        let p1 = document.createElement("p");
+        let p2 = document.createElement("p");
+        
+        if (item.answer === responses[i])
+        {
+            correct++;
+            p2.innerHTML = "Correct!";
+            li.classList.add("correct");
+        }
+        else
+        {
+            p2.innerHTML = "Wrong!";
+            li.classList.add("wrong");
+        }
+
+        p1.innerHTML = responses[i];
+
+        li.appendChild(p1);
+        li.appendChild(p2);
+        quiz_results_list.appendChild(li);
+    });
+    
+    let temp = parseFloat((correct) / quiz.length) * 100;
+    quiz_score.innerHTML = `${temp}%`;
+}
+
+function render()
+{
+    if (current === -1)
+    {
+        quiz_body.hidden = true;
+        quiz_retry_button.hidden = true;
+        quiz_exit_button.hidden = true;
+        quiz_build_button.hidden = false;
+    }
+    else if (current < quiz.length)
+    {
+        quiz_stats.innerHTML = `${current+1} / ${quiz.length}`;
+        question = quiz[current];
+        displayCurrentQuestion();
+    }
+    else
+    {
+        quiz_body.hidden = true;
+        quiz_retry_button.hidden = false;
+        quiz_results_list.hidden = false;
+        quiz_score.hidden = false;
+        displayResults();
+    }
+}
+
+document.addEventListener("DOMContentLoaded", main);
+
+// The following comes from: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+/* Randomize array in-place using Durstenfeld shuffle algorithm */
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
